81326e1253d1662b40eea837fdb9e48cf5f7a81b
bashar0151 <b.chriebati@live.com>
2022-12-27 12:56:57 +0000

    Rewrie with much better formatting!

--- index.js
+++ index.js
@@ -1,85 +1,32 @@
 // Require the necessary discord.js classes
 const fs = require('node:fs'); // Reads the commands directory and identify the command files
 const path = require('node:path'); // Helps construct paths to access files and directories. 
-const { Client, Events, GatewayIntentBits, EmbedBuilder, PermissionsBitField, Permissions, Collection } = require('discord.js'); // Collection is used to store and efficiently retrieve commands for execution
-const { token } = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');// Collection is used to store and efficiently retrieve commands for execution
+const { token } = require('./config.json');  // pulls token form another file for security
+const prefix = '/'; // Sets prefix bot used to pick up commands
 
-// Sets prefix bot used to pick up commands
-const prefix = '/';
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds,GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-
-/* When the client is ready, run this code (only once)
- We use 'c' for the event parameter to keep it separate from the already defined 'client'
-Sets bots activity status under Discord name
-*/
-
-client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}`);
-
-	client.user.setActivity(`Being Bashar's Virtual Son!`, { type: "WATCHING"});
-});
-
-// Log in to Discord with your client's token
-client.login(token);
-
-
-//Setups command handlear taks prefction and action typed command by user
-client.on("messageCreate", (message) => {
-
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
-
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
-
-	//Message array
-
-	const messageArray = message.content.split("");
-    const argument = messageArray.slice(1);
-	const cmd = messageArray[0];
-	
-	// Commands
-
-	// Tesy command
-
-	if (command === 'test') {
-
-		message.channel.send("Bot is working")
-	}
-})
-
-client.commands = new Collection();
-
 const commandsPath = path.join(__dirname, 'commands'); //  helps to construct a path to the commands directory
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js')); // readdirSync reads the path to the directory and returns an array of all the file names it contains & Array.filter() is used to ensure only command files get processed removes any non-JavaScript files from the array.
 
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+	const command = require(filePath); // Set a new item in the Collection with the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
 }
 
-client.on(Events.InteractionCreate, interaction => { //Receiving command interactions
-	if (!interaction.isChatInputCommand()) return;
-	console.log(interaction);
+client.once(Events.ClientReady, () => {
+	console.log('Ready!');
 });
 
-client.on(Events.InteractionCreate, async interaction => { //Executing commands
+client.on(Events.InteractionCreate, async interaction => { // Listening for users command input
 	if (!interaction.isChatInputCommand()) return;
 
-	const command = interaction.client.commands.get(interaction.commandName);
+	const command = client.commands.get(interaction.commandName);
 
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
+	if (!command) return;
 
 	try {
 		await command.execute(interaction);
@@ -89,3 +36,8 @@ client.on(Events.InteractionCreate, async interaction => { //Executing commands
 	}
 });
 
+/* When the client is ready, run this code (only once)
+ We use 'c' for the event parameter to keep it separate from the already defined 'client'
+Sets bots activity status under Discord name
+*/
+client.login(token);
